dee14b6e26f7cd03d4ab3d06e3c07ab13d562e5f
Romen <romanretinskiy@yandex.ru>
2022-11-30 19:04:42 +0300

    starting

---
+++ src/main/java/com/example/demo/HensolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HensolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HensolApplication.class, args);
+	}
+
+}
